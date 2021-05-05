40698de5c980fea1a6cb2eb54d6983b74a5127b2
Laaurent <48626779+Laaurent@users.noreply.github.com>
2021-05-05 20:13:58 +0200

    first commit

---
+++ components/NavbarComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="navbar_wrapper">
+    <div class="navbar_container">
+      <ul>
+        <li>Logo</li>
+        <li>Actualité</li>
+        <li>A propos</li>
+      </ul>
+      <ul>
+        <template v-if="!isLogged">
+          <li>Se connecter</li>
+          <li>S'inscrire</li>
+        </template>
+        <template v-else>
+          <li>IMG</li>
+        </template>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLogged: false
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/components/navbar.scss";
+</style>
