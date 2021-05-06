de5086fec4404699325ceddcba57f6f17989f013
Laurent <laurent@MacBook-Pro-de-Laurent.local>
2021-05-06 15:32:58 +0200

    register

--- pages/register.vue
+++ pages/register.vue
@@ -3,13 +3,31 @@
     <!-- FORMULAIRE -->
     <form action="" @submit.prevent="register">
       <label for="id">Identifiant</label>
-      <input type="text" name="id" id="id" placeholder=" - " />
+      <input
+        v-model="user.email"
+        type="text"
+        name="id"
+        id="id"
+        placeholder=" - "
+      />
 
       <label for="password">Mot de passe</label>
-      <input type="password" name="password" id="password" placeholder=" - " />
+      <input
+        v-model="user.password"
+        type="password"
+        name="password"
+        id="password"
+        placeholder=" - "
+      />
 
       <label for="password2">Confirmation mot de passe</label>
-      <input type="password" name="password" id="password2" placeholder=" - " />
+      <input
+        v-model="user.password2"
+        type="password"
+        name="password"
+        id="password2"
+        placeholder=" - "
+      />
 
       <input type="submit" value="S'inscrire" />
     </form>
@@ -17,10 +35,28 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
+  data() {
+    return {
+      user: {
+        email: undefined,
+        password: undefined,
+        password2: undefined,
+      },
+    };
+  },
   methods: {
     register() {
       console.log("register");
+      axios
+        .post(
+          `http://localhost:3000/register`,
+          { email: this.user.email, password: this.user.password },
+          { withCredentials: true }
+        )
+        .then((response) => console.log(response));
     },
   },
 };
