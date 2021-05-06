5bf6854e8a1a1bd7e56cdc263ede38eb6c1fdcf7
Laurent <laurent@MacBook-Pro-de-Laurent.local>
2021-05-06 15:22:50 +0200

    login

--- pages/login.vue
+++ pages/login.vue
@@ -1,13 +1,56 @@
 <template>
-  <div>LOGIN</div>
+  <div>
+    <!-- FORMULAIRE -->
+    <form action="" @submit.prevent="login">
+      <label for="id">Identifiant</label>
+      <input
+        v-model="user.email"
+        type="text"
+        name="id"
+        id="id"
+        placeholder=" - "
+      />
+
+      <label for="password">Mot de passe</label>
+      <input
+        v-model="user.password"
+        type="password"
+        name="password"
+        id="password"
+        placeholder=" - "
+      />
+
+      <input type="submit" value="Se connecter" />
+    </form>
+  </div>
 </template>
 
 <script>
-export default {
+import axios from "axios";
 
-}
+export default {
+  data() {
+    return {
+      user: {
+        email: undefined,
+        password: undefined,
+      },
+    };
+  },
+  methods: {
+    login() {
+      axios
+        .post(
+          `http://localhost:3000/login`,
+          { email: this.user.email, password: this.user.password },
+          { withCredentials: true }
+        )
+        .then((response) => console.log(response));
+    },
+  },
+};
 </script>
 
 <style>
-
+  @import "@/assets/scss/components/form.scss";
 </style>
---
+++ pages/register.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- FORMULAIRE -->
+    <form action="" @submit.prevent="register">
+      <label for="id">Identifiant</label>
+      <input type="text" name="id" id="id" placeholder=" - " />
+
+      <label for="password">Mot de passe</label>
+      <input type="password" name="password" id="password" placeholder=" - " />
+
+      <label for="password2">Confirmation mot de passe</label>
+      <input type="password" name="password" id="password2" placeholder=" - " />
+
+      <input type="submit" value="S'inscrire" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    register() {
+      console.log("register");
+    },
+  },
+};
+</script>
+
+<style>
+  @import "@/assets/scss/components/form.scss";
+</style>
