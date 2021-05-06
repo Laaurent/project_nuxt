e586884c570a68e83b66c3e4a3e16d0ec7b13579
Florian PIAULT <florian.piault@mail-ecv.fr>
2021-05-06 13:41:34 +0200

    correction "loose" config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,52 +1,43 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'project_nuxt',
+    title: "project_nuxt",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~/assets/scss/_variables.scss',
-    '~/assets/scss/knacss/knacss.scss'
-  ],
+  css: ["~/assets/scss/_variables.scss", "~/assets/scss/knacss/knacss.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-     '@nuxtjs/style-resources',
-     "@nuxtjs/auth",
-     "@nuxtjs/axios"
-  ],
+  modules: ["@nuxtjs/style-resources", "@nuxtjs/auth", "@nuxtjs/axios"],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    babel: {
+      plugins: [["@babel/plugin-proposal-private-methods", { loose: true }]]
+    }
   },
 
   //* MODULES
 
   styleResources: {
-    scss: ['./assets/scss/*.scss']
-  },
-  
-}
+    scss: ["./assets/scss/*.scss"]
+  }
+};
