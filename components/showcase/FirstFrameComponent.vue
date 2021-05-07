cc2ea892ddf480645bc41279f2f747810d02b127
Laurent <laurent@MacBook-Pro-de-Laurent.local>
2021-05-08 01:26:30 +0200

    index page

---
+++ components/showcase/FirstFrameComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="first_frame">
+    <div class="title">
+      <h1>LOREM IPSUM</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem
+        iusto tempore dolore vel nobis vero, saepe asperiores nesciunt sapiente
+        consectetur totam quaerat dignissimos aspernatur quisquam suscipit
+        ratione corporis fugiat ea.
+      </p>
+    </div>
+    <div class="head_news">
+      <ul>
+        <li>news 1</li>
+        <li>news 2</li>
+        <li>news 3</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ components/showcase/SecondFrameComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="second_frame">
+    <div class="wrapper_second_frame">
+      <div>1</div>
+      <div>2</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
