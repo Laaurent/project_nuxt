9af0397a5bb421a7ecaddfa8c5c4e2b27e4250b6
Laaurent <48626779+Laaurent@users.noreply.github.com>
2021-05-08 18:55:00 +0200

    second frame showcase

--- components/showcase/SecondFrameComponent.vue
+++ components/showcase/SecondFrameComponent.vue
@@ -1,8 +1,25 @@
 <template>
   <div class="second_frame">
     <div class="wrapper_second_frame">
-      <div>1</div>
-      <div>2</div>
+      <div class="img">
+        <div class="button_swipe_div">
+          <button class="button_swipe"><i class="fas fa-arrow-left"></i></button
+          ><button class="button_swipe">
+            <i class="fas fa-arrow-right"></i>
+          </button>
+        </div>
+      </div>
+      <div class="text">
+        <div>
+          <h2>LOREM IPSUM</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet
+            eligendi quaerat nihil delectus magni fugit architecto totam
+            aspernatur iste consequuntur. Nisi voluptas ea repellendus tempore.
+            Soluta tempore iure asperiores incidunt!
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,5 +28,4 @@
 export default {};
 </script>
 
-<style>
-</style>
+<style></style>
