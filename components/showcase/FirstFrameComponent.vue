1ac975ee1a22a969a3adeabdf053ced890613257
Laurent <laurent@MacBook-Pro-de-Laurent.local>
2021-05-09 23:38:10 +0200

    footer + background-image

--- components/showcase/FirstFrameComponent.vue
+++ components/showcase/FirstFrameComponent.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="first_frame">
-    <div class="title">
-      <h1>LOREM IPSUM</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem
-        iusto tempore dolore vel nobis vero, saepe asperiores nesciunt sapiente
-        consectetur totam quaerat dignissimos aspernatur quisquam suscipit
-        ratione corporis fugiat ea.
-      </p>
-    </div>
-    <div class="head_news">
-      <ul>
-        <li>news 1</li>
-        <li>news 2</li>
-        <li>news 3</li>
-      </ul>
+  <div class="first_frame_wrapper">
+    <div class="first_frame">
+      <div class="title">
+        <h1>LOREM IPSUM</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem
+          iusto tempore dolore vel nobis vero, saepe asperiores nesciunt
+          sapiente consectetur totam quaerat dignissimos aspernatur quisquam
+          suscipit ratione corporis fugiat ea.
+        </p>
+      </div>
+      <div class="head_news">
+        <ul>
+          <li>news 1</li>
+          <li>news 2</li>
+          <li>news 3</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
