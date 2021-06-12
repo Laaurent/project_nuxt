a5192a3d267d61054bcea5057ad7cfd65cdf3af7
Soliman <43494167+SolimanGit@users.noreply.github.com>
2021-06-12 16:15:20 +0200

    first_commit

--- pages/new/_id.vue
+++ pages/new/_id.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="main">
     <div class="details_card" v-if="new_item">
+      <div class="details_wrapper">
+        <div class="details_header">
+          <div><h1>{{ new_item.title}}</h1></div>
+          <div><h3>{{new_item.published}}</h3></div>
+        </div>
+      <div><img :src="new_item.img" alt="news_image"><p>{{ new_item.content }}</p></div>
+      
       {{ new_item }}
+      </div>
     </div>
   </div>
 </template>
